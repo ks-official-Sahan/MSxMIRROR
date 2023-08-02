3683a5c81981c09c34811acec30a6fcbb7efffff
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-08-02 21:18:02 +0530

    Create datas.py

---
+++ bot/modules/datas.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient 
+
+class primedb: 
+     def __init__(self, name, collection_name): 
+         self.client = MongoClient('mongodb+srv://paid:paid@cluster0.xgucvek.mongodb.net/?retryWrites=true&w=majority') 
+         self.db = self.client[name] 
+         self.collection = self.db[collection_name] 
+     def update_shortner(self,id,shortner):
+         userdata = dict(id=id, shortner=None) 
+         self.collection.insert_one(userdata) 
+         self.collection.update_one({"id": id}, {"$set": {'shortner':shortner}})
+     def shortner(self, id):
+         user = self.collection.find_one({'id': int(id)}) 
+         return user.get('shortner', None) 
+         
+db = primedb('prime','primee')
