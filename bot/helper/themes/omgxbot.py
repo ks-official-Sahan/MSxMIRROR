09564f2711486a659d4ec62cfc39f07864d822b6
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-08-03 23:05:45 +0530

    Deleteing

--- bot/helper/themes/omgxbot.py
+++
@@ -1,8 +0,0 @@
-class WZMLStyle:
-  # USER Setting --> user_setting.py 
-  USER_SETTING = '''㊂ <b><u>User Settings :</u></b>
-        
-<b> ɴᴀᴍᴇ :</b> {NAME} ( <code>{ID}</code> )
-<b> ᴜsᴇʀ ɴᴀᴍᴇ :</b> {USERNAME}
-<b> ᴛᴇʟᴇɢʀᴀᴍ ᴅᴄ :</b> {DC}
-<b> ʟᴀɴɢᴜᴀɢᴇ :</b> {LANG}'''
