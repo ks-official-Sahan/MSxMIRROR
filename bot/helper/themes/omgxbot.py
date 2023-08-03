e152161b967f82c2d7d0b341dba2eb95dd979057
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-08-03 15:34:55 +0530

    Create omgxbot.py

---
+++ bot/helper/themes/omgxbot.py
@@ -0,0 +1,8 @@
+class WZMLStyle:
+  # USER Setting --> user_setting.py 
+  USER_SETTING = '''㊂ <b><u>User Settings :</u></b>
+        
+<b> ɴᴀᴍᴇ :</b> {NAME} ( <code>{ID}</code> )
+<b> ᴜsᴇʀ ɴᴀᴍᴇ :</b> {USERNAME}
+<b> ᴛᴇʟᴇɢʀᴀᴍ ᴅᴄ :</b> {DC}
+<b> ʟᴀɴɢᴜᴀɢᴇ :</b> {LANG}'''
